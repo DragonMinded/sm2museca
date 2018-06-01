16ab616bab39b34a0e4dc80ffda74073ad2e5913
Mark LeMoine <thatsgobbles@gmail.com>
2018-06-01 13:44:10 -0700

    Seems tempfile workaround and associated methods are no longer needed

--- misc.py
+++ misc.py
@@ -1,6 +1,3 @@
-import os
-import tempfile
-
 class Constants:
     EVENT_KIND_NOTE = 0
     EVENT_KIND_HOLD = 1
@@ -16,27 +13,3 @@ class Constants:
 
     EVENT_KIND_GRAFICA_SECTION_START = 14
     EVENT_KIND_GRAFICA_SECTION_END = 15
-
-
-class TempFile:
-
-    def __init__(self, suffix: str='') -> None:
-        self.__suffix = suffix
-
-    def __enter__(self) -> str:
-        # Workaround for windows.
-        self.__tempfile = tempfile.NamedTemporaryFile(
-            suffix=self.__suffix,
-            delete=False,
-        )
-        return self.__tempfile.name
-
-    def __exit__(self, *args) -> None:
-        os.remove(self.__tempfile.name)
-
-
-def read_file(name: str) -> bytes:
-    fp = open(name, 'rb')
-    bv = fp.read()
-    fp.close()
-    return bv
